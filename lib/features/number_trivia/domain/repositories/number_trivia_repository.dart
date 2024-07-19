9281157f2d1bc23228d455986443b48f7ce811f0
Santiago Silvestre <silvestre.santiago732@gmail.com>
2024-07-19 12:05:10 -0300

    first comimt with all content

---
+++ lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
@@ -0,0 +1,8 @@
+import 'package:trivia/core/error/failure.dart';
+import 'package:trivia/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class NumberTriviaRepository {
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number);
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia();
+}
